bee8c6621f470ac3647998f8dd4eef4179cd23bf
epa <epa@localhost.localdomain>
2017-12-15 15:22:29 +0000

    add comments

--- week10/runtest.sh
+++ week10/runtest.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-time_length=5
+
+time_length=5 #Length of time for script to run for
 
 
-
-
-if [ $1 -lt 1 ] || [ $1 -gt 50 ]; then
+if [ $1 -lt 1 ] || [ $1 -gt 50 ]; then # if statement to check if number of users is in the required restraints
 echo "Users must be within a 1 and 50"
 else 
-     echo -e  "C0 \t N \t idle " >  results.dat
-     for (( i=1; i<=$1; i++ ))
+     echo -e  "C0 \t N \t idle " >  results.dat #entering headers for .dat file before running loop
+	#Run for loop based on how many users that hvae been entered by the parameter
+     for (( i=1; i<=$1; i++ ))		
      do
-	./loadtest $i &
-	idle=$(mpstat $time_length 1 -o JSON | jq ' .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle')
-	comp=$(cat synthetic.dat | wc -l)
+	./loadtest $i & #runs loadtest in the background
+	idle=$(mpstat $time_length 1 -o JSON | jq ' .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') #collect cpu utilisaion by pasring the json format and store in variable called idle
+	comp=$(cat synthetic.dat | wc -l) #Retreives the line count from synthetic.dat and stores in comp varible
 	
 
-echo -e "$comp \t $i \t $idle" >> results.dat
+echo -e "$comp \t $i \t $idle" >> results.dat # Move the results into results.dat file.
 
-pkill loadtest
+pkill loadtest #kill loadtest process
 
-done
+done # end loop
 
-fi 
+fi #end if statement 
 
